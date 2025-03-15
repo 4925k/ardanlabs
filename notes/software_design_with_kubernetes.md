451bcb6ef011aa8c8e4fc6f88d98092a60dd59b4
4925k <dibekpoudyal@gmail.com>
2025-03-15 07:52:57 +0000

    sdwk ep 3, 4 project base + logging

--- notes/software_design_with_kubernetes.md
+++ notes/software_design_with_kubernetes.md
@@ -5,3 +5,9 @@
 - deploy first mentality 
     - helps to test and debug
 - project structure
+
+# EP 3
+- logging
+    - levels to be skipped. dont have time to change log levels on production when required.
+- polymorphism -> a peice of code changes its behavior depending on the concrete type of data it is working on
+- always return concrete values
---
+++ software_design_with_kubernetes/service/app/services/sales-api/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/4925k/ardanlabs/software_design_with_kubernetes/service/foundation/logger"
+)
+
+func main() {
+	// LOGGING
+	var log *logger.Logger
+
+	events := logger.Events{
+		Error: func(ctx context.Context, r logger.Record) {
+			log.Info(ctx, "********* SEND ALERT *********")
+		},
+	}
+
+	traceIDFn := func(ctx context.Context) string {
+		return ""
+	}
+
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES_API", traceIDFn, events)
+
+	// --------------------------------------------------------------------------------------
+	// RUN
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "msg", err)
+		return
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+	// --------------------------------------------------------------------------------------
+	// GOMAXPROCS
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// --------------------------------------------------------------------------------------
+	// SHUTDOWN
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+}
