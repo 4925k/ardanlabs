cbbed4dda0eb374000c7b52acc4a51accdc6f6d6
4925k <dibekpoudyal@gmail.com>
2025-03-17 09:45:12 +0000

    startup, shutdown, configuration, debug endpoints

--- software_design_with_kubernetes/service/app/services/sales/main.go
+++ software_design_with_kubernetes/service/app/services/sales/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"context"
+	"errors"
+	"expvar"
+	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
+	"time"
 
+	"github.com/4925k/ardanlabs/software_design_with_kubernetes/service/business/web/v1/debug"
 	"github.com/4925k/ardanlabs/software_design_with_kubernetes/service/foundation/logger"
+	"github.com/ardanlabs/conf/v3"
 )
 
-var build = "develop "
+var build = "develop"
 
 func main() {
 	// LOGGING
@@ -41,10 +48,70 @@ func main() {
 func run(ctx context.Context, log *logger.Logger) error {
 	// --------------------------------------------------------------------------------------
 	// GOMAXPROCS
+
 	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "build", build)
+	expvar.NewString("build").Set(build)
+
+	// --------------------------------------------------------------------------------------
+	// CONFIGURATION
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s,mask"` // noprint tage hides it from the help, mask hide the value
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "4925k",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// --------------------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+
+	log.Info(ctx, "startup", "config", out)
+
+	// --------------------------------------------------------------------------------------
+	// Debug Service
+
+	go func() {
+		log.Info(ctx, "startup", "status", "debug v1 router started", "host", cfg.Web.DebugHost)
+
+		// NOTE:
+		// http.DefaultServeMux will host any paths that might be initialized by other packages
+		// this leaves us with unknown paths
+		// to address this we create a custom mux that only hosts the paths we care about
+		if err := http.ListenAndServe(cfg.Web.DebugHost, debug.Mux()); err != nil {
+			log.Error(ctx, "shutdown", "status", "debug v1 router closed", "host", cfg.Web.DebugHost, "msg", err)
+		}
+	}()
 
 	// --------------------------------------------------------------------------------------
 	// SHUTDOWN
+
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 	sig := <-shutdown
--- software_design_with_kubernetes/service/go.mod
+++ software_design_with_kubernetes/service/go.mod
@@ -1,3 +1,8 @@
 module github.com/4925k/ardanlabs/software_design_with_kubernetes/service
 
 go 1.22.2
+
+require (
+	github.com/ardanlabs/conf v1.5.0
+	github.com/ardanlabs/conf/v3 v3.4.0
+)
